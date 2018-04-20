a30fca8443df21cd44f35ce27dd125667b145d2d
Sarah <sarkinsvii@gmail.com>
2018-04-20 16:24:40 +0100

    added input args checks, return 3-char class label, accept input args in any case

--- RecogniseFace.py
+++ RecogniseFace.py
@@ -5,6 +5,31 @@ import glob
 from classification import identify_faces
 
 def RecogniseFace(input_path,feature,model):
+    """
+    Function detects and classifies faces in a given image, or in all images in a given folder
+    :param input_path: individual jpg file or folder containing jpg files
+    :param feature: feature type. Accepted arguments: 'surf', 'hog', 'lbp', 'None'
+    :param model: model type. Accepted arguments: 'mlp', 'rf', 'nb', 'svm', 'cnn'
+    :return:
+    """
+    # Check input args ok
+    if model=='cnn' and feature !='none':
+        print('If using model CNN, feature type must be "None"')
+        return
+
+    if model!='cnn' and feature=='none':
+        print('Feature type "none" only compatible with model type "cnn"')
+        return
+
+    models = ['mlp', 'surf', 'rf', 'nb', 'cnn']
+    if model not in models:
+        print('Model type "{}" not accepted. Must be one of: {}'.format(model, models))
+        return
+
+    feature_types = ['surf', 'lbp', 'hog', 'none']
+    if feature not in feature_types:
+        print('Feature type "{}" not accepted. Must be one of: {}'.format(feature, feature_types))
+        return
 
     results={}
     if not os.path.isdir(input_path):
@@ -30,19 +55,15 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         script_dir=os.path.dirname(sys.argv[0])
         input_path = sys.argv[1]
-        feature_type = sys.argv[2]
-        classification_method = sys.argv[3]
+        feature_type = sys.argv[2].lower()
+        classification_method = sys.argv[3].lower()
     else:
         script_dir=''
-        input_path=r"C:\Users\sarki\Pictures\nicole_kidman.jpg"
-        feature_type='None'
-        classification_method='cnn'
+        input_path=r"C:\Data\computer_vision_coursework\Images\Group11of11\Group11of11\IMG_0613_62.jpg"
+        feature_type='lbp'
+        classification_method='nb'
 
     if script_dir!='':
         os.chdir(script_dir)
-
-    # input_folder_root=r"U:\Data\computer_vision_coursework\face_images\from_group_photos\val\*"
-    # for folder in glob.glob(input_folder_root):
-    #     RecogniseFace(folder, feature_type, classification_method)
 
     RecogniseFace(input_path, feature_type, classification_method)
